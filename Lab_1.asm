ebce2de8f02737372ba0beb3f70d1fefeb827b7a
AsKorch <alex.c0r@yandex.ru>
2026-03-04 18:05:45 +0300

    Add files via upload

--- Lab_1.asm
+++ Lab_1.asm
@@ -1,104 +1,68 @@
-; Программа для вычисления Y = |A - 3*B| / (B*B)
-; С подробным выводом
+.intel_syntax noprefix
 
-extern printf
-extern scanf
-extern exit
+.section .data
+prompt_a: .asciz "Enter A: "
+prompt_b: .asciz "Enter B: "
+format_in: .asciz "%d"
+format_out: .asciz "Result = %f\n"
+error_msg: .asciz "B cannot be zero!\n"
 
-section .data
-    promptA db "Enter A: ", 0
-    promptB db "Enter B: ", 0
-    inputMsg db "A = %d, B = %d", 10, 0
-    resultMsg db "Y = %.3f", 10, 0
-    errorMsg db "Error: Division by zero!", 10, 0
-    formatInt db "%d", 0
+.section .bss
+a: .space 4
+b: .space 4
 
-section .bss
-    A resd 1
-    B resd 1
-    resultFloat resq 1
-
-section .text
-    global main
+.section .text
+.globl main
+.extern printf
+.extern scanf
 
 main:
-    push ebp
-    mov ebp, esp
+    push rbp
+    mov rbp, rsp
+    sub rsp, 32
 
-    ; Ввод A
-    push promptA
+    lea rcx, prompt_a
     call printf
-    add esp, 4
-    
-    push A
-    push formatInt
+    lea rdx, a
+    lea rcx, format_in
     call scanf
-    add esp, 8
 
-    ; Ввод B
-    push promptB
+    lea rcx, prompt_b
     call printf
-    add esp, 4
-    
-    push B
-    push formatInt
+    lea rdx, b
+    lea rcx, format_in
     call scanf
-    add esp, 8
 
-    ; Проверка B на 0
-    mov eax, [B]
-    cmp eax, 0
-    je .error
+    mov eax, dword ptr [a]
+    mov ebx, dword ptr [b]
 
-    ; Выводим введенные значения
-    push dword [B]
-    push dword [A]
-    push inputMsg
-    call printf
-    add esp, 12
+    cmp ebx, 0
+    je error
 
-    ; Вычисляем |A - 3B|
-    mov eax, [B]
-    imul eax, 3         ; eax = 3B
-    mov ebx, [A]
-    sub ebx, eax        ; ebx = A - 3B
+    mov ecx, ebx
+    imul ebx, ebx, 3
+    sub eax, ebx
     
-    ; Модуль
-    mov eax, ebx
-    cmp eax, 0
-    jge .abs_done
-    neg eax
-.abs_done:
-    push eax            ; сохраняем |A-3B| в стек
+    cdq
+    xor eax, edx
+    sub eax, edx
 
-    ; Вычисляем B^2
-    mov eax, [B]
-    imul eax, eax       ; eax = B^2
-    push eax            ; сохраняем B^2 в стек
-
-    ; Загружаем в FPU для точного деления
-    fild dword [esp+4]  ; ST(0) = |A-3B|
-    fild dword [esp]    ; ST(0) = B^2, ST(1) = |A-3B|
-    fdivp               ; ST(0) = |A-3B| / B^2
+    cvtsi2sd xmm0, eax
+    imul ecx, ecx
+    cvtsi2sd xmm1, ecx
+    divsd xmm0, xmm1
     
-    ; Сохраняем результат
-    fstp qword [resultFloat]
-    
-    add esp, 8          ; очищаем стек от сохраненных значений
-
-    ; Выводим результат
-    push dword [resultFloat+4]
-    push dword [resultFloat]
-    push resultMsg
+    lea rcx, format_out
+    movq rdx, xmm0
     call printf
-    add esp, 12
+    jmp done
 
-    push 0
-    call exit
-
-.error:
-    push errorMsg
+error:
+    lea rcx, error_msg
     call printf
-    add esp, 4
-    push 1
-    call exit
+
+done:
+    xor eax, eax
+    mov rsp, rbp
+    pop rbp
+    ret
